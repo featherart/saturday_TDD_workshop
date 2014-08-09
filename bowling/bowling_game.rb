690a83e06830bfa3993637c4214f688693094dea
Feather <lisafeather@gmail.com>
2014-08-09 14:34:59 -0700

    factor out game object

--- bowling/bowling_game.rb
+++ bowling/bowling_game.rb
@@ -1,4 +1,12 @@
 
-
 class BowlingGame
+  attr_reader :score
+
+  def initialize
+    @score = 0
+  end
+
+  def roll times
+    @score += times
+  end
 end
--- bowling/bowling_game_spec.rb
+++ bowling/bowling_game_spec.rb
@@ -1,4 +1,22 @@
 require_relative 'bowling_game'
 
 describe BowlingGame do
+
+  before do
+    @game = BowlingGame.new
+  end
+
+  it 'scores all gutters with 0' do
+
+    20.times { game.roll(0) }
+
+    expect(game.score).to eq(0)
+  end
+
+  it 'scores all 1s with 20' do
+
+    20.times { game.roll(1) }
+
+    expect(game.score).to eq(20)
+  end
 end
