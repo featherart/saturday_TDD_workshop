11d8574fcaaa6b5cdfb3b9910c938c640211d112
Feather <lisafeather@gmail.com>
2014-08-09 15:04:16 -0700

    refactor to use let()

--- bowling/game_spec.rb
+++ bowling/game_spec.rb
@@ -2,22 +2,24 @@ require_relative 'game'
 
 module Bowling
   describe Game do
+
+    let(:game) { Game.new }
+
     it "return 0 for a miss (no pins are knocked down)" do
-      game = Game.new
       game.miss
 
       expect(game.score).to eq(0)
     end
 
     it "return 10 for a strike (knocking down all ten pins)" do
-      game = Game.new
+
       game.strike
 
       expect(game.score).to eq(10)
     end
 
     it "return the number of pins hit for a spare" do
-      game = Game.new
+
       game.spare(8)
 
       expect(game.score).to eq(8)
@@ -25,7 +27,7 @@ module Bowling
 
     it "when a strike is bowled, the total score is 10 +
         the total of the next two roll to that frame" do
-      game = Game.new
+       
       game.strike
 
       game.roll(7)
